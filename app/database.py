f8c25363a90fd1080c6dc88206cf54ba1cfd121c
Sevasya <sloskutov2003@gmail.com>
2025-10-28 20:15:18 +0300

    Initial commit

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import func
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
+
+
+DATABASE_URL = 'sqlite+aiosqlite:///db.sqlite3'
+engine = create_async_engine(url=DATABASE_URL)
+async_session_maker = async_sessionmaker(bind=engine, class_=AsyncSession)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
+    
